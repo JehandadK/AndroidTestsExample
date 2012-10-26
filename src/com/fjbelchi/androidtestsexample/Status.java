03cc25a261317c22c932ed7dc7699c7f4b321ee6
Fran <fjbelchi@gmail.com>
2012-10-26 11:50:40 +0200

    singleton ready to test

---
+++ src/com/fjbelchi/androidtestsexample/Status.java
@@ -0,0 +1,14 @@
+package com.fjbelchi.androidtestsexample;
+
+public class Status {
+
+    private String status;
+
+    public Status() {
+        status = "status";
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
